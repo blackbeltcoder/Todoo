137230cc7b6f2e97dbbe55e738971922f215374c
blackbeltcoder <sep.russell@gmail.com>
2016-10-13 12:49:09 +0100

    initial

---
+++ src/main/java/com/sar/sandpit/TodooApplication.java
@@ -0,0 +1,12 @@
+package com.sar.sandpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodooApplication.class, args);
+	}
+}
