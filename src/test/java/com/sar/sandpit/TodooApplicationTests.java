54898f71652391a35dd7473b3141f90aa363f6e9
blackbeltcoder <sep.russell@gmail.com>
2016-10-13 14:04:32 +0100

    test

--- src/test/java/com/sar/sandpit/TodooApplicationTests.java
+++ src/test/java/com/sar/sandpit/TodooApplicationTests.java
@@ -1,9 +1,16 @@
 package com.sar.sandpit;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +20,18 @@ public class TodooApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void addTaskItem_success() throws Exception {
+
+		String s ="";
+
+		s=null;
+		///Assert.assertThat("",);
+
+		assertThat("", s ,is(notNullValue()));
+
+
+
+
+	}
 }
